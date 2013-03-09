274e92ffafaf86af599898af65f32a65dc106a87
Jeff Carpenter <gcarpenterv@gmail.com>
2013-03-09 00:27:06 -0800

    So fresh and so clean

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thecolbyecho::Application.config.secret_token = '7790e6db853ef576530007539b6b756d67bd43e4721037097246b35b1bd9b536ce21f87611e369cb8729e36305222e5ce3f58834fa00d037d1d04a6f14bbbe60'
